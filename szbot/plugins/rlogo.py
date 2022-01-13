2f23919d62b379a0253e170d9e8b55158452f284
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-01-13 10:35:24 +0530

    Update rlogo.py

--- szbot/plugins/rlogo.py
+++ szbot/plugins/rlogo.py
@@ -72,7 +72,7 @@ def nospace(s):
     return s
 
 
-@sz.on_message(filters.command(["rlogo", f"rlogo@szimagebot"]) & ~filters.edited & ~filters.bot)
+@sz.on_message(filters.command(["rlogo", f"rlogo@NiupunDinujaya_bot"]) & ~filters.edited & ~filters.bot)
 async def logogen(client, message):
     pablo = await client.send_message(message.chat.id,"`Creating The Logo.....`")
     Godzilla = nospace(message.text.strip().split(None, 1)[1].lower())
@@ -91,10 +91,10 @@ async def logogen(client, message):
     imgcaption = f"""
 ☘️**Random Logo Created Successfully**✅
 ◇───────────────◇
-🔥 **Created by** : @szimagebot
-⚡️ **Powered By **  : `【SZ™】`
+🔥 **Created by** : @NiupunDinujaya_bot
+⚡️ **Powered By **  : `【 NIPUN 】`
 ◇───────────────◇
-©2021[【SZ™】 team ](https://t.me/szteambots) **All Right Reserved**⚠️️
+©2022 **All Right Reserved**⚠️️
 """
     created = "logo.jpg"
     await client.send_photo(message.chat.id, photo = created, caption = imgcaption, reply_markup = repmark )
